7c69319b1bceabd4ebb99905175fe6eac4988903
Jorge <jorge019912@gmail.com>
2020-10-23 08:23:59 -0500

    Detalles de php_category

--- admin/models/category.php
+++ admin/models/category.php
@@ -13,31 +13,31 @@ $estado = (isset($_POST['opc_estado'])) ? $_POST['opc_estado'] : '';
 
 switch($opcion){
     case 1:
-        $consulta = "CALL sp_createAssignCategory('$id_trabajador','$id_categoria','$estado');";	
+        $consulta = "CALL sp_create_assing_category('$id_trabajador','$id_categoria','$estado');";	
         $resultado = $conexion->prepare($consulta);
         $resultado->execute();             
         $data = ($resultado->rowCount()==1) ? array("msj"=>"success") : array("msj"=>"error");
         break;
     case 2:
-        $consulta = "CALL sp_updateAssignCategory('$id','$id_trabajador','$id_categoria','$estado'); ";		
+        $consulta = "CALL sp_update_assing_category('$id','$id_trabajador','$id_categoria','$estado'); ";		
         $resultado = $conexion->prepare($consulta);
         $resultado->execute();
         $data = ($resultado->rowCount()==1) ? array("msj"=>"success") : array("msj"=>"error");
         break;        
     case 3:
-        $consulta = "CALL sp_deleteAssignCategory('$id');";		
+        $consulta = "CALL sp_delete_assing_category('$id');";		
         $resultado = $conexion->prepare($consulta);
         $resultado->execute();
         $data = ($resultado->rowCount()==1) ? array("msj"=>"success") : array("msj"=>"error");
         break;         
     case 4:
-        $consulta = "CALL sp_readAssignCategory()";
+        $consulta = "CALL sp_read_assing_category()";
         $resultado = $conexion->prepare($consulta);
         $resultado->execute();
         $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
         break;
     case 5:
-        $consulta = "CALL sp_readCategory()";
+        $consulta = "CALL sp_read_category()";
         $resultado = $conexion->prepare($consulta);
         $resultado->execute();
         $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
